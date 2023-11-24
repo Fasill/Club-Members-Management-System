11b50bf6c847599a5074dbf0c50426ba972481ba
fasil <fasilhawultie19@gmail.com>
2023-11-24 10:09:46 +0300

    fix: Resolve syntax error in firebaseMiddleware.js Addressed a syntax error in firebaseMiddleware.js by correcting the import statement for serviceAccount from ../credentials/serviceAccountKey.json. The file path or import format was causing a 'SyntaxError: Unexpected identifier'.

--- backend/middleware/firebaseMiddleware.js
+++ backend/middleware/firebaseMiddleware.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin'
-import serviceAccount from "../credentials/serviceAccountKey.json"  assert { type: "json" };
+import serviceAccount from "../credentials/serviceAccountKey.json";
 
 if (!admin.apps.length) {
     admin.initializeApp({
